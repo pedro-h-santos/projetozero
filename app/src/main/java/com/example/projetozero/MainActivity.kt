6990064d91f190ac87853b23bc140fbc1392ebc3
Pedro Santos <pedro.henriq.sants@gmail.com>
2023-05-22 22:14:44 -0300

    segundo commit

--- app/src/main/java/com/example/projetozero/MainActivity.kt
+++ app/src/main/java/com/example/projetozero/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.projetozero.ui.theme.ProjetozeroTheme
-
+//teste 1 para git commmit
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
